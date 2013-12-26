251d638063b85b6fa5c7e2dc1bbaa605b040040d
azuchi <azuchi@haw.co.jp>
2013-12-26 10:30:31 +0900

    hh:mm PST以外のフォーマットがあったのでそのフォーマットに対応。

--- extention.js
+++ extention.js
@@ -5,10 +5,15 @@ $(function(){
     var params = div.attr("id").split("_");
     var dateStr = params[params.length - 1];
     // 時刻とタイムゾーン（PST）
-    var time = $.trim($(this).html()); // TODO DOMにセットされる時刻のフォーマットが違う場合がある。
+    var time = $.trim($(this).html());
     // 時間を算出
-    var date = new Date(Date.parse(
-        dateStr.substring(0, 4) + "/" + dateStr.substring(4, 6) + "/" + dateStr.substring(6, 9) + " " + time));
+    var date;
+    if($.isNumeric(time.substr(0, 1))){ // hh:mm PST フォーマットの場合
+      date = new Date(Date.parse(
+          dateStr.substring(0, 4) + "/" + dateStr.substring(4, 6) + "/" + dateStr.substring(6, 9) + " " + time));
+    } else{
+      date = new Date(Date.parse(time)); // Dec dd, hh:mm AM PST フォーマットの場合
+    }
     var dateElements = date.toString().split(" ");
     var converted = date.getFullYear() + "/" + date.getMonth() + "/" + date.getDate() + " " +
       date.getHours() + ":" + date.getMinutes() + " " + dateElements[dateElements.length - 2] + " " + dateElements[dateElements.length - 1];
