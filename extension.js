c12d88dd1a55bcee03d30ccc4612fa5255995335
azuchi <azuchi@haw.co.jp>
2014-01-04 01:28:52 +0900

    current statusの日時変換時にDOMの構造が異なりエラーになってたのでひとまずエラー回避。

--- extension.js
+++ extension.js
@@ -1,23 +1,37 @@
 $(function(){
   $("span.yellowfg").each(function(){
     // 対象の日付（yyyyMMdd）を算出
-    var div = $(this).parent().parent().parent().parent();
-    var params = div.attr("id").split("_");
-    var dateStr = params[params.length - 1];
-    // 時刻とタイムゾーン（PST）
-    var time = $.trim($(this).html());
-    // 時間を算出
-    var date;
-    if($.isNumeric(time.substr(0, 1))){ // hh:mm PST フォーマットの場合
-      date = new Date(Date.parse(
+    var me = $(this);
+    if(isCurrentStatus(me)) {
+
+    } else {
+      var div = me.parent().parent().parent().parent();
+      var params = div.attr("id").split("_");
+      var dateStr = params[params.length - 1];
+      // 時刻とタイムゾーン（PST）
+      var time = $.trim($(this).html());
+      // 時間を算出
+      var date;
+      if($.isNumeric(time.substr(0, 1))){ // hh:mm PST フォーマットの場合
+        date = new Date(Date.parse(
           dateStr.substring(0, 4) + "/" + dateStr.substring(4, 6) + "/" + dateStr.substring(6, 9) + " " + time));
-    } else{
-      date = new Date(Date.parse(time)); // Dec dd, hh:mm AM PST フォーマットの場合
+      } else{
+        date = new Date(Date.parse(time)); // Dec dd, hh:mm AM PST フォーマットの場合
+      }
+      var dateElements = date.toString().split(" ");
+      var converted = (date.getMonth()+1) + "/" + date.getDate() + " " +
+        date.getHours() + ":" + date.getMinutes() + " " + dateElements[dateElements.length - 2] + " " + dateElements[dateElements.length - 1];
+      $(this).html(converted);
     }
-    var dateElements = date.toString().split(" ");
-    var converted = (date.getMonth()+1) + "/" + date.getDate() + " " +
-      date.getHours() + ":" + date.getMinutes() + " " + dateElements[dateElements.length - 2] + " " + dateElements[dateElements.length - 1];
-    $(this).html(converted);
   });
 });
 
+/**
+ * Current Statusに記載れているデータか、Status Historyに記載されているデータか判定します。
+ * @param targetElement
+ * @returns {boolean} Current Statusの場合はtrue
+ */
+function isCurrentStatus(targetElement){
+  var id = targetElement.parent().parent().attr("id");
+  return id != undefined && id.indexOf("current_") != -1;
+}
